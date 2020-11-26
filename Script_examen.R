8f06c13f8e569a01196086e07234601b5e2cb1e9
Guillaume Mulier <guillaume.mulier62220@gmail.com>
2020-11-27 00:17:18 +0100

    Finition des K-means et de la CAH

--- Script_examen.R
+++ Script_examen.R
@@ -6,6 +6,7 @@
 
 #### Packages ----
 
+library(conflicted)
 library(tidyverse)
 library(patchwork)
 library(ggtext)
@@ -13,9 +14,15 @@ library(FactoMineR)
 library(factoextra)
 library(dendextend)
 library(purrr)
+library(scales)
+library(mclust)
 
 theme_set(theme_light() +
             theme(plot.title = element_markdown()))
+
+conflict_prefer(name = "filter", winner = "dplyr")
+conflict_prefer(name = "summarise", winner = "dplyr")
+conflict_prefer("map", "purrr")
 
 #### Partie classification non supervisée ----
 
@@ -39,9 +46,13 @@ les_inconnus %>%
   pivot_wider(names_from = "stat")
 # Variables autour de 0 avec des unités assez proches, même si certaines un peu plus grandes
 
+les_inconnus_réduits <- les_inconnus %>% 
+  mutate(across(everything(), .fns = ~ (.x - mean(.x)) / sd(.x)))
+# Variables réduites
+
 ## ACP
 
-ACP_inc <- PCA(les_inconnus, scale.unit = TRUE, graph = FALSE)
+ACP_inc <- PCA(les_inconnus_réduits, scale.unit = TRUE, graph = FALSE)
 fviz_screeplot(ACP_inc, addlabels = TRUE)
 # les 2 1ers axes captent la quasi-totalité de l'inertie de l'échantillon donc on ne regardera que ces 2 1ers axes
 plot_axes <- fviz_pca_var(ACP_inc, repel = TRUE) +
@@ -58,9 +69,9 @@ plot_ind + plot_axes
 ## CAH
 
 # Dendrogramme en 3 classes
-dendro <- les_inconnus %>% 
+dendro <- les_inconnus_réduits %>% 
   dist(method = "euclidean") %>% 
-  hclust(method = "ward.D2")
+  hclust(method = "ward.D2") # Distance euclidienne avec aggrégation pour clusters sphériques
 dendro_marches <- cbind(x = 1:500, inertie = sort(dendro$height, decreasing = TRUE), color = c(1, 1, 1, rep(2, 497))) %>% 
   as.data.frame() %>% 
   slice(1:20) %>% 
@@ -113,11 +124,11 @@ plot_ind2 + plot_axes
 
 # Détermination du nombre de clusters
 
-replication_kmeans <- function(data, n_rep, nb_clust, result = "modele") {
+replication_kmeans <- function(data, n_rep, nb_clust, result = "modele", nstart) {
   
   result <- match.arg(result, c("modele", "liste"))
   
-  kmeans_rep <- replicate(n_rep, kmeans(data, centers = nb_clust, nstart = 25))
+  kmeans_rep <- replicate(n_rep, kmeans(data, centers = nb_clust, nstart = nstart))
   best <- which.min(unlist(kmeans_rep['tot.withinss', ]))
   kmeans <- kmeans_rep[, best]
   
@@ -131,9 +142,10 @@ replication_kmeans <- function(data, n_rep, nb_clust, result = "modele") {
 modeles <- vector(mode = "list", length = 10)
 for (i in 1:10) {
   cat(i, "\n")
-  modeles[[i]] <- replication_kmeans(data = les_inconnus, nb_clust = i, n_rep = 25)
+  modeles[[i]] <- replication_kmeans(data = les_inconnus_réduits,
+                                     nb_clust = i, n_rep = 25, nstart = 25)
 }
-map_dfr(modeles, ~ c(intra = .x[["tot.withinss"]]), .id = "k") %>% 
+plot_inertie <- map_dfr(modeles, ~ c(intra = .x[["tot.withinss"]]), .id = "k") %>% 
   bind_cols(color = c(1, 1, 1, rep(2, 7))) %>% 
   ggplot(aes(x = as.numeric(k), y = intra)) +
   geom_point(aes(color = as.factor(color)), show.legend = FALSE) +
@@ -146,9 +158,26 @@ map_dfr(modeles, ~ c(intra = .x[["tot.withinss"]]), .id = "k") %>%
        title = "Décroissance de l'inertie avec le nombre de clusters",
        subtitle = "Sélection de la meilleure classification parmi 25 réplicats à chaque k") +
   theme(panel.grid.minor.x = element_blank())
+# Là encore, il y a un saut dans les inerties pour k = 3 environ
+plot_calinski <- map_dfr(modeles, ~ c(intra = .x[["tot.withinss"]], inter = .x[["betweenss"]]), .id = "k") %>% 
+  mutate(k = as.numeric(k),
+         calinski = ifelse(k == 1, 0, ((500 - k) * inter) / ((k - 1) * intra)),
+         color = c(2, 2, 1, rep(2, 7))) %>% 
+  ggplot(aes(x = k, y = calinski)) +
+  geom_point(aes(color = as.factor(color)), show.legend = FALSE) +
+  geom_line() +
+  geom_vline(xintercept = 3, color = "blue", linetype = "dashed") +
+  scale_color_manual(values = c("blue", "black")) +
+  scale_x_continuous(breaks = 1:10) +
+  labs(x = "Nombre de clusters",
+       y = "Indice de Calinski-Harabasz",
+       title = "Evolution de l'indice de Calinski-Harabasz en fonction du nombre de clusters faits") +
+  theme(panel.grid.minor.x = element_blank())
+# k = 3 maximise l'indice de calinski
+plot_inertie + plot_calinski
 
 # Choix de k = 3
-kmeans_replicats <- replication_kmeans(data = les_inconnus, nb_clust = 3, n_rep = 25, result = "liste")
+kmeans_replicats <- replication_kmeans(data = les_inconnus_réduits, nb_clust = 3, n_rep = 25, result = "liste", nstart = 1)
 kmeans_replicats[1, ] %>% 
   map_dfr(~ c(clust = .x)) %>% 
   t() %>% 
@@ -163,9 +192,14 @@ kmeans_replicats[7, ] %>%
   ggplot(aes(rep, value, color = name)) +
   geom_point() +
   geom_line() +
-  coord_cartesian(ylim = c(140, 180))
+  scale_color_discrete(name = "Clusters :", labels = c("1", "2", "3")) +
+  coord_cartesian(ylim = c(140, 180)) +
+  labs(x = "Numéro de réplication",
+       y = "Effectif",
+       title = "Vérification de la taille des clusters en réplicant 25 fois les K-means")
 # Les effectifs dans les clusters sont relativement stables et équilibrés
 
+# Décompte des clusters, ne marche pas pour les ex aequos
 for (i in 2:25) {
   tableau <- table(kmeans_replicats[, 1]$cluster, kmeans_replicats[, i]$cluster)
   num_1 <- as.numeric(which.max(tableau[, 1]))
@@ -177,7 +211,7 @@ for (i in 2:25) {
                                              TRUE ~ NA_real_) 
 }
 liste_clusters <- kmeans_replicats[1, ] %>% 
-  map_dfr(~ c(clust = .x)) %>% 
+  map_dfr(~ c(ind = .x)) %>% 
   t() %>% 
   as.data.frame() %>% 
   apply(1, function(x) {paste(x, collapse = "")}) %>% 
@@ -185,13 +219,50 @@ liste_clusters <- kmeans_replicats[1, ] %>%
 count(liste_clusters, seq, sort = TRUE)
 liste_clusters %>% 
   mutate(seq = ifelse(str_detect(seq, "^(1+|2+|3+)$"), "Mêmes clusters", "Clusters différents")) %>% 
-  count(seq)
+  count(seq) %>%  
+  mutate(prop = paste0(100 * round(n / sum(n), 1), "%"))
 # Pas l'air d'y avoir des clusters qui sont différents parmi les réplicats
 
+# Matrice de similarité
+n <- 500
+mat <-  matrix(0, nrow = n, ncol = n) # matrice carrée de taille n de 0
+clusters <- kmeans_replicats[1, ] %>% 
+  map_dfr(~ c(ind = .x)) %>% 
+  t() %>% 
+  as.data.frame()
+clusters <- clusters[order(clusters$V1), ]
+# Tri du tableau selon le 1er vecteur de clusters
+for (r in seq_len(ncol(clusters))) {
+  classe <- clusters[, r]
+  for (i in 1:(n-1)) {
+    for(j in (i + 1):n) {
+      if (classe[i] == classe[j]) mat[i, j] <- mat[i, j] + 1
+    }
+  }
+}
+image(mat)
+(mat / ncol(clusters)) %>% 
+  as.data.frame() %>% 
+  rownames_to_column() %>% 
+  pivot_longer(-rowname) %>% 
+  mutate(rowname = as.numeric(rowname),
+         name = as.numeric(str_remove(name, "^V"))) %>% 
+  ggplot(aes(rowname, name, fill = value)) +
+  geom_tile() +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_fill_gradient2(low = "white", mid = "green", high = "blue", 
+                       midpoint = 0.5, labels = percent_format(),
+                       name = NULL) +
+  labs(x = NULL,
+       y = NULL) +
+  annotate(geom = "richtext", x = 250, y = 200, size = 3, hjust = 0.2,
+           label = "**Visualisation des individus triés selon la <br> 1<sup>ère</sup> réplication de K-means :<br>Pourcentage de fois où les 2 individus sont <br>dans le même cluster sur les 25 réplicats**")
+
 # Représentation
-kmeans_3 <- replication_kmeans(data = les_inconnus, nb_clust = 3, n_rep = 25, result = "mode")
+kmeans_3 <- replication_kmeans(data = les_inconnus_réduits, nb_clust = 3, n_rep = 25, result = "mode", nstart = 25)
 class(kmeans_3) <- "kmeans"
-plot_ind3 <- fviz_cluster(kmeans_3, les_inconnus, ellipse.type = "convex",
+plot_ind3 <- fviz_cluster(kmeans_3, les_inconnus_réduits, ellipse.type = "convex",
                           geom = "point", ggtheme = theme_light()) +
   labs(title = "Projection des individus sur les 2 1<sup>ers</sup> axes de l'ACP",
        subtitle = "Colorés selon les groupes faits par méthode K-means") +
@@ -200,7 +271,14 @@ plot_ind3 <- fviz_cluster(kmeans_3, les_inconnus, ellipse.type = "convex",
         legend.position = "none")
 plot_ind3 + plot_axes
 
+les_inconnus_shopenhauer$class_kmeans <- kmeans_3$cluster
+
 ## Modèle de mélanges Gaussiens
+
+# A la vue de l'ACP, choix de modèle Gaussiens sphériques (et donc VII/EII pour l'opposer aux K-means)
+boules_gaus <- map(1:20, ~ Mclust(les_inconnus, G = .x, modelNames = c("VII", "EII")))
+boules_gaus[[1]]$classification
+plot(boules_gaus, what = "BIC")
 
 #### Partie classification supervisée ----
 
